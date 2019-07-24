6afa1c6cddb2c5bc4cbdfd384e1e31db54e5779c
文亮 <shishandejiagoubiji@gmail.com>
2019-07-24 17:48:08 +0800

    add .

--- src/main/java/com/jdk/data/structures/jdkdatastructures/JdkDatastructuresApplication.java
+++ src/main/java/com/jdk/data/structures/jdkdatastructures/JdkDatastructuresApplication.java
@@ -1,5 +1,4 @@
 package com.jdk.data.structures.jdkdatastructures;
-
 /**
  * @author wenliang
  */
---
+++ src/main/java/com/jdk/data/structures/jdkdatastructures/wenliang/arraylist/ArrayList.java
@@ -0,0 +1,40 @@
+package com.jdk.data.structures.jdkdatastructures.wenliang.arraylist;
+
+/**
+ *  手写ArrayList
+ * @author wenliang
+ */
+public class ArrayList <E> {
+
+    /**
+     * 元素的数量
+     */
+    private int size;
+
+    /**
+     * 所有的元素数量
+     */
+    private E[] datas;
+
+    /**
+     * 初始化容量
+     */
+    private static final int DEFAULT_CAPACITY = 10;
+
+
+
+    public ArrayList(int capaticy){
+        capaticy = (capaticy < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capaticy;
+        this.datas = (E[]) new Object[capaticy];
+    }
+
+    /***
+     * 默认构造方法
+     */
+    public ArrayList(){
+        this(DEFAULT_CAPACITY);
+
+    }
+
+
+}
