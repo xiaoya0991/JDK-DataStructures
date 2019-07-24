f32dffb5e35a45a6fe41b3fbaa2c5ddf407329d3
文亮 <shishandejiagoubiji@gmail.com>
2019-07-24 13:13:02 +0800

    first commit

---
+++ src/main/java/com/jdk/data/structures/jdkdatastructures/JdkDatastructuresApplication.java
@@ -0,0 +1,16 @@
+package com.jdk.data.structures.jdkdatastructures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wenliang
+ */
+@SpringBootApplication
+public class JdkDatastructuresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdkDatastructuresApplication.class, args);
+    }
+
+}
