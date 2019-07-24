636165f649f7799e7822e4806d2fcc7cb0daca17
文亮 <shishandejiagoubiji@gmail.com>
2019-07-24 13:39:45 +0800

    去除无用的依赖

--- src/main/java/com/jdk/data/structures/jdkdatastructures/JdkDatastructuresApplication.java
+++ src/main/java/com/jdk/data/structures/jdkdatastructures/JdkDatastructuresApplication.java
@@ -1,16 +1,13 @@
 package com.jdk.data.structures.jdkdatastructures;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * @author wenliang
  */
-@SpringBootApplication
+
 public class JdkDatastructuresApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JdkDatastructuresApplication.class, args);
+
     }
 
 }
