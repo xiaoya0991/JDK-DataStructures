274e2fd3d9d8f5f2c89b67ef936bdc1b1a03c0d1
wulimax <1772003186@qq.com>
2019-07-24 17:37:39 +0800

    'go-1'

--- src/main/java/com/jdk/data/structures/jdkdatastructures/wlimax/LiArrayList.java
+++ src/main/java/com/jdk/data/structures/jdkdatastructures/wlimax/LiArrayList.java
@@ -1,13 +1,6 @@
 package com.jdk.data.structures.jdkdatastructures.wlimax;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+i
 public class LiArrayList {
 
-    @GetMapping("/index")
-    private void index(){
-        System.out.println(111);
-    }
 }
