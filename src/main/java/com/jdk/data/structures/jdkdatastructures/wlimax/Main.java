652b4bdfd1dafe5934a4a3c44d79794dfa05be28
wulimax <1772003186@qq.com>
2019-08-06 17:39:56 +0800

    'del'

--- src/main/java/com/jdk/data/structures/jdkdatastructures/wlimax/Main.java
+++ src/main/java/com/jdk/data/structures/jdkdatastructures/wlimax/Main.java
@@ -1,21 +1,10 @@
 package com.jdk.data.structures.jdkdatastructures.wlimax;
 
 
-
 public class Main {
 
     public static void main(String[] args) {
-        LinkList arr = new LinkList();
-//        arr.addFirse(1);
-//        arr.addFirse(2);
-        arr.addFirse(1);
-        arr.addFirse(2);
-        arr.addFirse(3);
-        arr.addFirse(4);
-        arr.addFirse(5);
-        arr.addLast(6);
-        System.out.println(arr);
-        System.out.println(arr.get(4));
+
 
     }
     private void liArrayTest(){
