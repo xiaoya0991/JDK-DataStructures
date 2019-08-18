fa9aa18f763ec77fca8d69b32e1a13ec7e161c79
shengjieForever <shengjie_forever@163.com>
2019-08-18 22:30:54 +0800

    二叉搜索树新增元素的方法

--- src/main/java/com/jdk/data/structures/jdkdatastructures/shengjie/tree/BinarySearchTree.java
+++ src/main/java/com/jdk/data/structures/jdkdatastructures/shengjie/tree/BinarySearchTree.java
@@ -1,7 +1,7 @@
 package com.jdk.data.structures.jdkdatastructures.shengjie.tree;
 
 /**
- * 二叉搜索树
+ * binary search tree without same element
  */
 public class BinarySearchTree<E extends Comparable<E>>{
     private class Node{
@@ -29,5 +29,56 @@ public class BinarySearchTree<E extends Comparable<E>>{
 
     public boolean isEmpty(){
         return size == 0;
+    }
+
+//    /**
+//     * add new element
+//     * @param e
+//     */
+//    public void add(E e){
+//        if(root == null){
+//            root = new Node(e);
+//            size ++;
+//        }else
+//            add(root,e);
+//    }
+//    //add new element to the binary search tree that the root node with root
+//    private void add(Node node, E e){
+//        if(e.equals(node.e))
+//            return;
+//        else if(e.compareTo(node.e) < 0 && node.left == null){
+//            node.left = new Node(e);
+//            size ++;
+//            return;
+//        } else if(e.compareTo(node.e) > 0 && node.right == null){
+//            node.right = new Node(e);
+//            size ++;
+//            return;
+//        }
+//
+//        if(e.compareTo(node.e) < 0)
+//            add(node.left, e);
+//        else
+//            add(node.right, e);
+//    }
+
+    /**
+     * add new element to the binary search tree that the root node with root by recursion method
+     */
+    public void add(E e){
+        add(root,e);
+    }
+    private Node add(Node node, E e){
+        if(node == null){
+            size ++;
+            return new Node(e);
+        }
+
+        if(e.compareTo(node.e) < 0)
+            node.left = add(node.left, e);
+        else if(e.compareTo(node.e) > 0)
+            node.right = add(node.right, e);
+
+        return node;
     }
 }
