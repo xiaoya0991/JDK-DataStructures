2892bd7cf689a7cfaf702cb20ef6ff0e6fdfe308
文亮 <shishandejiagoubiji@gmail.com>
2019-09-13 08:32:20 +0800

    fix

--- src/main/java/com/jdk/data/structures/jdkdatastructures/wenliang/tree/AVLTree.java
+++ src/main/java/com/jdk/data/structures/jdkdatastructures/wenliang/tree/AVLTree.java
@@ -1,7 +1,22 @@
 package com.jdk.data.structures.jdkdatastructures.wenliang.tree;
 
+import java.util.Stack;
+
 /**
  * @author wenliang
  */
 public class AVLTree<E> extends BST<E> {
+
+
+    public int evalRPN(String[] tokens){
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens){
+            switch (token){
+                case "+":
+
+            }
+
+        }
+    }
 }
