2dcf1e61f890bf0e548e571b9f0044bd68f4d052
文亮 <shishandejiagoubiji@gmail.com>
2019-09-22 10:28:51 +0800

    fix

--- src/main/java/com/jdk/data/structures/jdkdatastructures/wenliang/tree/BinarySearchTree.java
+++ src/main/java/com/jdk/data/structures/jdkdatastructures/wenliang/tree/BinarySearchTree.java
@@ -9,10 +9,8 @@ import java.util.Queue;
  *
  * @param <E>
  */
-public class BinarySearchTree <E> {
+public class BinarySearchTree <E>  extends BinaryTree{
 
-    private int size;
-    private Node<E> root;
     private Comparator<E> comparator;
 
 
@@ -23,15 +21,18 @@ public class BinarySearchTree <E> {
     }
 
 
+
     public BinarySearchTree(Comparator<E> comparator){
         this.comparator = comparator;
     }
+
 
 
     /***
      *
      * @return
      */
+    @Override
     public int size(){
         return size;
     }
@@ -41,6 +42,7 @@ public class BinarySearchTree <E> {
      *
      * @return
      */
+    @Override
     public boolean isEmpty(){
         return size==0;
     }
@@ -49,135 +51,15 @@ public class BinarySearchTree <E> {
     /**
      *
      */
+    @Override
     public void clera(){
         root = null;
         size = 0;
-
-
     }
 
 
-    /**
-     * 前序遍历
-     */
-    public void preorderTraversal(){
-
-    }
-
-    private void preorderTraversal(Node<E> node){
-
-        if (node == null) return;
 
 
-        System.out.println(node.element);
-        preorderTraversal(node.left);
-        preorderTraversal(node.right);
-    }
-
-
-    /***
-     * 中序遍历
-     */
-    public void inorderTraversal(){
-        inorderTraversal(root);
-    }
-
-    private void inorderTraversal(Node<E>  node){
-
-        if (node == null) return;
-
-
-        inorderTraversal(node.left);
-        System.out.println(node.element);
-        inorderTraversal(node.right);
-
-    }
-
-
-    /***
-     * 中序遍历
-     */
-    public void postorderTraversal(){
-        inorderTraversal(root);
-    }
-
-    private void postorderTraversal(Node<E>  node){
-
-        if (node == null) return;
-
-
-        inorderTraversal(node.left);
-        inorderTraversal(node.right);
-        System.out.println(node.element);
-
-    }
-
-
-    /**
-     * 层序遍历
-     */
-    public void levelOrderTraversal(){
-
-        if (root == null) return;
-
-
-        Queue<Node<E>> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()){
-            Node<E> node = queue.poll();
-            System.out.println(node.element);
-
-            if (node.left != null){
-                queue.offer(node.left);
-            }
-
-            if (node.right != null) {
-                queue.offer(node.right);
-            }
-        }
-    }
-
-
-    /**
-     *
-     * @param visitor
-     */
-    public void levelOrder(Visitor<E> visitor){
-        if (root == null || visitor == null) return;
-
-
-        Queue<Node<E>> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()){
-            Node<E> node = queue.poll();
-           visitor.visit(node.element);
-
-            if (node.left != null){
-                queue.offer(node.left);
-            }
-
-            if (node.right != null) {
-                queue.offer(node.right);
-            }
-        }
-
-    }
-
-
-    public void preOrder(Visitor<E> visitor){
-        this.preOrder(visitor,root);
-
-    }
-
-    private void preOrder(Visitor<E> visitor,Node<E> node){
-
-        visitor.visit(node.element);
-        preOrder(visitor,node.left);
-        preOrder(visitor,node.right);
-
-    }
 
 
     public boolean isComplete(){
@@ -210,19 +92,7 @@ public class BinarySearchTree <E> {
     }
 
 
-    public static interface Visitor<E> {
-        void visit(E element);
-    }
 
-
-    public int height(){
-        return height(root);
-    }
-
-    private int height(Node<E> node){
-
-        return 1 + Math.max(height(node.left), height(node.right));
-    }
 
 
 
@@ -274,60 +144,7 @@ public class BinarySearchTree <E> {
     }
 
 
-    /**
-     *
-     * @param node
-     *
-     * @return
-     */
-    private Node<E> predesessor(Node<E> node){
-        if (node.left == null) return node;
 
-
-        Node<E> p = node.left;
-        if (p != null) {
-            while (p.right != null) {
-                p = p.right;
-            }
-            return p;
-        }
-
-
-        while (node.parent !=null && node== node.parent){
-            node = node.parent;
-        }
-
-        return node.parent;
-
-    }
-
-
-    /**
-     *
-     * @param node
-     *
-     * @return
-     */
-    private Node<E> successor(Node<E> node){
-        if (node == null) return node;
-
-
-        Node<E> p =node.right;
-        if (p != null){
-            while (p.left != null){
-                p = p.left;
-            }
-            return p;
-        }
-
-
-        while (node.parent != null && node == node.parent.right){
-            node = node.parent;
-        }
-
-        return node.parent;
-
-    }
 
 
 
@@ -469,32 +286,7 @@ public class BinarySearchTree <E> {
 
 
 
-    /**
-     *
-     * @param <E>
-     */
-    private static class Node<E> {
-        E element;
-        Node<E> left;
-        Node<E> right;
-        Node<E> parent;
 
-        public Node(E element,Node parent){
-            this.element = element;
-            this.parent = parent;
-        }
-
-
-        public boolean isLeaf(){
-            return left == null && right == null;
-        }
-
-
-        public boolean hasTwoChildren(){
-            return left != null && right != null;
-        }
-
-    }
 
 
 
