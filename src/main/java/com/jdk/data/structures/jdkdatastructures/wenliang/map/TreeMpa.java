841da26d09ed68ebf8cfd7442c4e165bff361b4c
文亮 <1203344821@qq.com>
2020-02-03 09:44:38 +0800

    TreeMpa完善put方法的编写

--- src/main/java/com/jdk/data/structures/jdkdatastructures/wenliang/map/TreeMpa.java
+++ src/main/java/com/jdk/data/structures/jdkdatastructures/wenliang/map/TreeMpa.java
@@ -1,27 +1,83 @@
 package com.jdk.data.structures.jdkdatastructures.wenliang.map;
 
+import java.util.Comparator;
+
 public class TreeMpa<K,V> implements Map<K,V>{
     private static final boolean RED = false;
     private static final boolean BLACK= true;
+    private Node<K,V> root;
+    private int size;
+    private Comparator<K> comparator;
+
+    public TreeMpa(){
+        this(null);
+    }
+
+    public TreeMpa(Comparator<K> comparator){
+        this.comparator= comparator;
+    }
 
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public void clear() {
+        this.root = null;
+        this.size = 0;
 
     }
 
     @Override
     public V put(K key, V value) {
+        keyNotNullCheck(key);
+
+        //添加的第一个节点
+        if (root == null) {
+            root = new Node<>(key, value, null);
+            this.size++;
+
+            //添加节点之后的处理
+            afterPut(root);
+            return null;
+        }
+
+        //添加的不是第一个节点
+        //找到父节点
+        Node<K, V> parent = root;
+        Node<K,V> node = root;
+        int cmp = 0;
+        while (node != null){
+            cmp = compare(key, node.key);
+            if (cmp > 0){
+                node = node.right;
+            }else if (cmp <0){
+                node = node.left;
+            }
+            //相等,覆盖
+            node.key = key;
+            V oldValue = node.value;
+            node.value = value;
+            return oldValue;
+        }
+
+        //插入到父节点的那个位置
+        Node<K, V> newNode = new Node<>(key, value, parent);
+        if (cmp > 0){
+            parent.right = newNode;
+        }else {
+            parent.left = newNode;
+        }
+        this.size++;
+        afterPut(newNode);
+
         return null;
     }
 
@@ -50,6 +106,23 @@ public class TreeMpa<K,V> implements Map<K,V>{
 
     }
 
+    private void keyNotNullCheck(K key){
+        if (key == null) {
+            throw new IllegalArgumentException("key must not be null")
+        }
+    }
+
+    private void afterPut(Node<K,V> node){
+
+    }
+
+    private int compare(K e1,K e2){
+        if (this.comparator != null){
+            return this.comparator.compare(e1, e2);
+        }
+        return ((Comparable<K>) e1).compareTo(e2);
+    }
+
     private static class Node<K,V> {
         K key;
         V value;
@@ -74,11 +147,6 @@ public class TreeMpa<K,V> implements Map<K,V>{
         public boolean isLeftChild(){
             return parent != null && this == parent.left;
         }
-
-
-
-
-
 
     }
 }
